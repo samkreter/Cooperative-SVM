c9d747de522e202083c82d036a62671929ca695c
Sam Kreter <samkreter@gmail.com>
2016-03-20 18:22:42 -0500

    added base structure for parser and svm

---
+++ parser.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def getNumpeeArray(filename):
+    return np.load(filename)
---
+++ svm.py
@@ -0,0 +1,20 @@
+
+class svm():
+
+    def __init__(self, arg):
+        self.arg = arg
+        a = langranging_multipliers()
+        b = getB()
+
+
+    def langranging_multipliers():
+        getGramMatrix()
+        getP
+        getQ
+        getA()
+
+
+
+    def getB():
+    def Kernal():
+    def _gramMatrix():
