274df8c246b68d7d2332e5f72dcf38b2cb6903b1
Sam Kreter <samkreter@gmail.com>
2016-03-24 05:02:00 -0500

    train test

--- train.py
+++ train.py
@@ -1,4 +1,10 @@
-import main
+import utils
+import sys
 
 def train(inputfileX, inputfileY):
-    trainAndStoreSvms(inputfileX, inputfileY, "test")
+    utils.trainAndStoreSvms(inputfileX, inputfileY, "test")
+
+
+if __name__ == '__main__':
+    if(len(sys.argv) >= 3):
+        train(sys.argv[1], sys.argv[0])
