508895d4a88f53d1447f4372154952f06db3fbe0
Sam Kreter <samkreter@gmail.com>
2016-03-25 11:10:53 -0500

    moved kernel functions into rapper class

--- coSvm/kernels.py
+++ coSvm/kernels.py
@@ -1,17 +1,19 @@
 import numpy as np
 import numpy.linalg as la
 
-def rbf(Sigma):
-    def k(x, y):
-        l2norm = la.norm(x - y)**2
-        return np.exp(-l2norm/(2*Sigma*Sigma))
-    return k
 
-def linear():
-    def k(x, y):
-        return np.inner(x,y)
-    return k
+class Kernel:
+    def rbf(Sigma):
+        def k(x, y):
+            l2norm = la.norm(x - y)**2
+            return np.exp(-l2norm/(2*Sigma*Sigma))
+        return k
 
-def gaussian(sigma):
-    return lambda x, y: \
-        np.exp(-np.sqrt(la.norm(x-y) ** 2 / (2 * sigma ** 2)))
+    def linear():
+        def k(x, y):
+            return np.inner(x,y)
+        return k
+
+    def gaussian(sigma):
+        return lambda x, y: \
+            np.exp(-np.sqrt(la.norm(x-y) ** 2 / (2 * sigma ** 2)))
